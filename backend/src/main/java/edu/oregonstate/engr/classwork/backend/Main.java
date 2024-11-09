2139c58c7d1a53d9d991bbe71b43f64b104c22ec
David K. <122331465+kazariad@users.noreply.github.com>
2024-11-08 17:27:54 -0800

    Use ISO date format for JSON

--- backend/src/main/java/edu/oregonstate/engr/classwork/backend/Main.java
+++ backend/src/main/java/edu/oregonstate/engr/classwork/backend/Main.java
@@ -1,35 +1,12 @@
 package edu.oregonstate.engr.classwork.backend;
 
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
-
-import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer customizer() {
-        // https://www.baeldung.com/spring-boot-formatting-json-dates#customizing-objectmapper
-        return builder -> {
-            builder.serializers(
-                    new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
-                    new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-            );
-            builder.deserializers(
-                    new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
-                    new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-            );
-        };
     }
 }
