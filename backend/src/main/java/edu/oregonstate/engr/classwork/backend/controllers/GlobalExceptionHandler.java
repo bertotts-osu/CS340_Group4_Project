b705e3ceaa83ae744f6682267a2a9aeb6e5a8127
David K. <122331465+kazariad@users.noreply.github.com>
2024-11-13 19:17:28 -0800

    Add WorkOrderEmployee validation

--- backend/src/main/java/edu/oregonstate/engr/classwork/backend/controllers/GlobalExceptionHandler.java
+++ backend/src/main/java/edu/oregonstate/engr/classwork/backend/controllers/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package edu.oregonstate.engr.classwork.backend.controllers;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.http.*;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +17,24 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler
     public ProblemDetail handleAllNonSpring(Exception ex) {
         return ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public ProblemDetail handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, "Invalid Entry state");
+    }
+
+    @ExceptionHandler
+    public ProblemDetail handleDuplicateKey(DuplicateKeyException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, "Entry already exists");
+    }
+
+    @ExceptionHandler
+    public ProblemDetail handleIncorrectResultSize(IncorrectResultSizeDataAccessException ex) {
+        if (ex.getExpectedSize() == 1 && ex.getActualSize() == 0) {
+            return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, "Entry doesn't exist");
+        }
+        throw ex;
     }
 
     @Override
--- backend/src/main/java/edu/oregonstate/engr/classwork/backend/models/WorkOrderEmployee.java
+++ backend/src/main/java/edu/oregonstate/engr/classwork/backend/models/WorkOrderEmployee.java
@@ -1,12 +1,17 @@
 package edu.oregonstate.engr.classwork.backend.models;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data //automatically generates getters and setters
 public class WorkOrderEmployee {
+    @Positive
     private int work_order_id;
+    @Positive
     private int employee_id;
+    @NotNull
     private LocalDateTime assigned_at;
 }
