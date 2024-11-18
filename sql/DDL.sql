5a1736bc5268a052e0bd4c2c20c9dd1b53d7424f
David K. <122331465+kazariad@users.noreply.github.com>
2024-11-17 18:39:40 -0800

    Update SQL

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -28,9 +28,12 @@ CREATE TABLE `WorkOrders` (
 	PRIMARY KEY (`work_order_id`)
 );
 -- Add the WorkOrders data
-INSERT INTO `WorkOrders` VALUES (1, 'Medium', '2520 NW Hayes Ave', 'Corvallis', 'OR', '97330', 'In Progress', '2024-08-04 15:45:56', '2024-08-15 12:25:42', '2024-08-23 09:45:23', '2024-09-02 08:45:41', NULL, NULL, NULL);
-INSERT INTO `WorkOrders` VALUES (2, 'Small', '1432 College St', 'Philomath', 'OR', '97370', 'Scheduled', '2024-08-20 12:23:35', '2024-08-25 16:45:04', '2024-08-30 11:34:52', NULL, NULL, NULL, NULL);
-INSERT INTO `WorkOrders` VALUES (3, 'Large', '4170 SW Research Way', 'Corvallis', 'OR', '97333', 'Applied', '2024-09-03 16:56:05', NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `WorkOrders`
+(`work_order_id`, `size`, `street`, `city`, `state`, `zip`, `stage`, `applied_at`, `estimated_at`, `scheduled_at`,`started_at`,`completed_at`,`on_hold_at`, `canceled_at`)
+VALUES
+(1, 'Medium', '2520 NW Hayes Ave', 'Corvallis', 'OR', '97330', 'In Progress', '2024-08-04 15:45:56', '2024-08-15 12:25:42', '2024-08-23 09:45:23', '2024-09-02 08:45:41', NULL, NULL, NULL),
+(2, 'Small', '1432 College St', 'Philomath', 'OR', '97370', 'Scheduled', '2024-08-20 12:23:35', '2024-08-25 16:45:04', '2024-08-30 11:34:52', NULL, NULL, NULL, NULL),
+(3, 'Large', '4170 SW Research Way', 'Corvallis', 'OR', '97333', 'Applied', '2024-09-03 16:56:05', NULL, NULL, NULL, NULL, NULL, NULL);
 
 
 
@@ -47,11 +50,14 @@ CREATE TABLE `Employees` (
 	PRIMARY KEY (`employee_id`)
 );
 -- Add the Employees data
-INSERT INTO `Employees` VALUES (1, 'Scott', 'Derickson', 's.derickson@lfeaves.com', '541-223-6245', 'Active', 'Principal');
-INSERT INTO `Employees` VALUES (2, 'Aria', 'Stark', 'a.stark@lfeaves.com', '555-123-4567', 'Active', 'Associate');
-INSERT INTO `Employees` VALUES (3, 'Thor', 'Odinsson', 't.odinsson@lfeaves.com', '555-987-6543', 'Inactive', 'Associate');
-INSERT INTO `Employees` VALUES (4, 'Luna', 'Larson', 'l.larson@lfeaves.com', '555-678-1234', 'Active', 'Principal');
-INSERT INTO `Employees` VALUES (5, 'Nick', 'Anderson', 'n.anderson@lfeaves.com', '555-456-7890', 'Active', 'Apprentice');
+INSERT INTO `Employees`
+(`employee_id`, `first_name`, `last_name`, `email`, `phone_number`, `status`, `skill_level`)
+VALUES
+(1, 'Scott', 'Derickson', 's.derickson@lfeaves.com', '541-223-6245', 'Active', 'Principal'),
+(2, 'Aria', 'Stark', 'a.stark@lfeaves.com', '555-123-4567', 'Active', 'Associate'),
+(3, 'Thor', 'Odinsson', 't.odinsson@lfeaves.com', '555-987-6543', 'Inactive', 'Associate'),
+(4, 'Luna', 'Larson', 'l.larson@lfeaves.com', '555-678-1234', 'Active', 'Principal'),
+(5, 'Nick', 'Anderson', 'n.anderson@lfeaves.com', '555-456-7890', 'Active', 'Apprentice');
 
 
 
@@ -66,9 +72,12 @@ CREATE TABLE `WorkOrderEmployees` (
 	FOREIGN KEY (`employee_id`) REFERENCES `Employees` (`employee_id`) ON DELETE CASCADE
 );
 -- Add the WorkOrderEmployees data
-INSERT INTO `WorkOrderEmployees` VALUES (1, 1, '2024-08-04 16:47:56');
-INSERT INTO `WorkOrderEmployees` VALUES (1, 5, '2024-08-05 17:46:25');
-INSERT INTO `WorkOrderEmployees` VALUES (2, 2, '2024-08-21 12:25:25');
+INSERT INTO `WorkOrderEmployees`
+(`work_order_id`, `employee_id`, `assigned_at`)
+VALUES
+(1, 1, '2024-08-04 16:47:56'),
+(1, 5, '2024-08-05 17:46:25'),
+(2, 2, '2024-08-21 12:25:25');
 
 
 
@@ -84,9 +93,12 @@ CREATE TABLE `PurchaseOrders` (
 	FOREIGN KEY (`work_order_id`) REFERENCES `WorkOrders` (`work_order_id`) ON DELETE SET NULL
 );
 -- Add the PurchaseOrders data
-INSERT INTO `PurchaseOrders` VALUES (1, '2024-08-01 15:23:53', 1, NULL);
-INSERT INTO `PurchaseOrders` VALUES (2, '2024-08-23 06:23:35', 4, 1);
-INSERT INTO `PurchaseOrders` VALUES (3, '2024-08-30 23:34:05', 4, 2);
+INSERT INTO `PurchaseOrders`
+(`purchase_order_id`, `created_at`, `employee_id`, `work_order_id`)
+VALUES
+(1, '2024-08-01 15:23:53', 1, NULL),
+(2, '2024-08-23 06:23:35', 4, 1),
+(3, '2024-08-30 23:34:05', 4, 2);
 
 
 
@@ -105,13 +117,16 @@ CREATE TABLE `PurchaseOrderItems` (
 	FOREIGN KEY (`material_id`) REFERENCES `Materials` (`material_id`) ON DELETE CASCADE
 );
 -- Add the PurchaseOrderItems data
-INSERT INTO `PurchaseOrderItems` VALUES (1, 2.10, 10000, '2024-08-08', 'Ship', 1, 3);
-INSERT INTO `PurchaseOrderItems` VALUES (2, 1.24, 10000, '2024-08-08', 'Ship', 1, 2);
-INSERT INTO `PurchaseOrderItems` VALUES (3, 5.49, 500, '2024-08-10', 'Ship', 1, 1);
-INSERT INTO `PurchaseOrderItems` VALUES (4, 2.10, 286, NULL, 'Stock', 2, 3);
-INSERT INTO `PurchaseOrderItems` VALUES (5, 5.56, 22, NULL, 'Stock', 2, 1);
-INSERT INTO `PurchaseOrderItems` VALUES (6, 2.15, 118, NULL, 'Stock', 3, 3);
-INSERT INTO `PurchaseOrderItems` VALUES (7, 5.56, 16, NULL, 'Stock', 3, 1);
+INSERT INTO `PurchaseOrderItems`
+(`purchase_order_item_id`, `unit_cost`, `quantity`, `estimated_delivery_date`, `delivery_type`, `purchase_order_id`, `material_id`)
+VALUES
+(1, 2.10, 10000, '2024-08-08', 'Ship', 1, 3),
+(2, 1.24, 10000, '2024-08-08', 'Ship', 1, 2),
+(3, 5.49, 500, '2024-08-10', 'Ship', 1, 1),
+(4, 2.10, 286, NULL, 'Stock', 2, 3),
+(5, 5.56, 22, NULL, 'Stock', 2, 1),
+(6, 2.15, 118, NULL, 'Stock', 3, 3),
+(7, 5.56, 16, NULL, 'Stock', 3, 1);
 
 
 
@@ -126,11 +141,14 @@ CREATE TABLE `Materials` (
 	PRIMARY KEY (`material_id`)
 );
 -- Add the Materials data
-INSERT INTO `Materials` VALUES (1, 'Bolt Fastener Set', 'EA', 5.56, 462);
-INSERT INTO `Materials` VALUES (2, 'Standard Seamless Eave 5\"', 'FT', 1.24, 10000);
-INSERT INTO `Materials` VALUES (3, 'Standard Seamless Eave 6\"', 'FT', 2.12, 9596);
-INSERT INTO `Materials` VALUES (4, 'Colored Seamless Eave 5\"', 'FT', 3.45, 0);
-INSERT INTO `Materials` VALUES (5, 'Colored Seamless Eave 6\"', 'FT', 4.15, 0);
+INSERT INTO `Materials`
+(`material_id`, `name`, `unit`, `unit_cost`, `quantity_available`)
+VALUES
+(1, 'Bolt Fastener Set', 'EA', 5.56, 462),
+(2, 'Standard Seamless Eave 5"', 'FT', 1.24, 10000),
+(3, 'Standard Seamless Eave 6"', 'FT', 2.12, 9596),
+(4, 'Colored Seamless Eave 5"', 'FT', 3.45, 0),
+(5, 'Colored Seamless Eave 6"', 'FT', 4.15, 0);
 
 
 
