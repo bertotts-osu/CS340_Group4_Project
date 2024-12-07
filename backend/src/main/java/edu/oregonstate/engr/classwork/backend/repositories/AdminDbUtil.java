eded90dbfc22c987694168c9ef10a8fcbc60a45a
David K. <122331465+kazariad@users.noreply.github.com>
2024-12-06 20:48:54 -0800

    Fix DB connection warnings

--- backend/src/main/java/edu/oregonstate/engr/classwork/backend/repositories/AdminDbUtil.java
+++ backend/src/main/java/edu/oregonstate/engr/classwork/backend/repositories/AdminDbUtil.java
@@ -1,14 +1,20 @@
 package edu.oregonstate.engr.classwork.backend.repositories;
 
+import edu.oregonstate.engr.classwork.backend.controllers.GlobalExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 import java.sql.SQLException;
 
 @Component
 public class AdminDbUtil {
+    private final Logger logger = LoggerFactory.getLogger(AdminDbUtil.class);
+
     private final DataSource dataSource;
     private final ResourceDatabasePopulator resourceDatabasePopulator;
 
@@ -17,9 +23,10 @@ public class AdminDbUtil {
         this.resourceDatabasePopulator = resourceDatabasePopulator;
     }
 
-    public void resetDb() {
-        try {
-            resourceDatabasePopulator.populate(dataSource.getConnection());
+    public synchronized void resetDb() {
+        try (Connection con = dataSource.getConnection()) {
+            resourceDatabasePopulator.populate(con);
+            logger.info("DB reset");
         } catch (SQLException ex) {
             throw new CannotGetJdbcConnectionException(ex.getMessage(), ex);
         }
